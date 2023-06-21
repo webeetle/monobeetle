acc6df434b35d3d256f1e0287e6f68cd0895d81d
Riccardo Tartaglia <tartaglia.riccardo@gmail.com>
2023-06-21 18:01:39 +0200

    generator project generate

--- libs/cherry/src/generators/service-e2e/generator.ts
+++ libs/cherry/src/generators/service-e2e/generator.ts
@@ -5,18 +5,35 @@ import {
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
-import { ServiceE2eGeneratorSchema } from './schema';
+import {ServiceE2eGeneratorSchema} from './schema';
 
 export async function serviceE2eGenerator(
   tree: Tree,
   options: ServiceE2eGeneratorSchema
 ) {
   const projectRoot = `apps/${options.name}-e2e`;
-  addProjectConfiguration(tree, options.name, {
+  addProjectConfiguration(tree, `${options.name}-e2e`, {
     root: projectRoot,
     projectType: 'application',
     sourceRoot: `${projectRoot}/src`,
-    targets: {},
+    targets: {
+      "e2e": {
+        "executor": "@nx/jest:jest",
+        "outputs": ["{workspaceRoot}/coverage/{e2eProjectRoot}"],
+        "options": {
+          "jestConfig": "apps/"+options.name+"-e2e/jest.config.ts",
+          "passWithNoTests": true
+        }
+      },
+      "lint": {
+        "executor": "@nx/linter:eslint",
+        "outputs": ["{options.outputFile}"],
+        "options": {
+          "lintFilePatterns": ["apps/"+options.name+"-e2e/**/*.{js,ts}"]
+        }
+      }
+    },
+    tags: ['type:service-e2e'],
   });
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
--- libs/cherry/src/generators/service/generator.ts
+++ libs/cherry/src/generators/service/generator.ts
@@ -16,7 +16,102 @@ export async function serviceGenerator(
     root: projectRoot,
     projectType: 'application',
     sourceRoot: `${projectRoot}/src`,
-    targets: {},
+    tags: ['type:service'],
+    targets: {
+      "build": {
+        "executor": "@nx/esbuild:esbuild",
+        "outputs": ["{options.outputPath}"],
+        "defaultConfiguration": "production",
+        "options": {
+          "platform": "node",
+          "outputPath": "dist",
+          "format": ["cjs"],
+          "bundle": false,
+          "main": "apps/"+options.name+"/src/main.ts",
+          "tsConfig": "apps/"+options.name+"/tsconfig.app.json",
+          "assets": [{ "input": "apps/"+options.name+"/src/assets", "glob": "*.*", "output": "/apps/"+options.name+"/src/assets" }],
+          "generatePackageJson": true,
+          "esbuildOptions": {
+            "sourcemap": true,
+            "outExtension": {
+              ".js": ".js"
+            }
+          }
+        },
+        "configurations": {
+          "development": {},
+          "production": {
+            "generateLockfile": true,
+            "esbuildOptions": {
+              "sourcemap": false,
+              "outExtension": {
+                ".js": ".js"
+              }
+            }
+          }
+        }
+      },
+      "serve": {
+        "executor": "@nx/js:node",
+        "defaultConfiguration": "development",
+        "options": {
+          "buildTarget": ""+options.name+":build"
+        },
+        "configurations": {
+          "development": {
+            "buildTarget": ""+options.name+":build:development"
+          },
+          "production": {
+            "buildTarget": ""+options.name+":build:production"
+          }
+        }
+      },
+      "db:init": {
+        "dependsOn": [
+          {
+            "projects": "self",
+            "target": "db:migrate:reset"
+          },
+          {
+            "projects": "self",
+            "target": "db:migrate:latest"
+          }
+        ],
+        "command": "echo 'initialized db'"
+      },
+      "db:migrate:reset": {
+        "command": "knex migrate:rollback --knexfile=apps/"+options.name+"/src/knexfile.ts --all"
+      },
+      "db:migrate:latest": {
+        "command": "knex migrate:latest --knexfile=apps/"+options.name+"/src/knexfile.ts"
+      },
+      "lint": {
+        "executor": "@nx/linter:eslint",
+        "outputs": ["{options.outputFile}"],
+        "options": {
+          "lintFilePatterns": ["apps/"+options.name+"/**/*.ts"]
+        }
+      },
+      "test": {
+        "executor": "@nx/jest:jest",
+        "outputs": ["{workspaceRoot}/coverage/{projectRoot}"],
+        "options": {
+          "jestConfig": "apps/"+options.name+"/jest.config.ts",
+          "runInBand": true,
+          "passWithNoTests": true
+        },
+        "configurations": {
+          "ci": {
+            "ci": true,
+            "codeCoverage": true
+          }
+        }
+      },
+      "docker-build": {
+        "dependsOn": ["build"],
+        "command": "docker build -f apps/"+options.name+"/Dockerfile . -t "+options.name+""
+      }
+    },
   });
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
   await formatFiles(tree);
